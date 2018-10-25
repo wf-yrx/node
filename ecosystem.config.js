575a4d09935411fa11010423528aa8325ebe2811
万峰 <iuliao@163.com>
2018-10-25 17:30:47 +0800

    第二次

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'node-test',
+    script: './app.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+        user : 'yueyagou',
+        host : '47.107.74.126',
+        ref  : 'origin/master',
+        port : '12178',
+        repo : 'git@github.com:wf-yrx/node.git',
+        path : '/home/yueyagou/www/wf',
+        'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
